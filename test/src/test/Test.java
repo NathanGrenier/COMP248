6b029a1184e3a53a9e9214f1cf080e41e6137395
Nathan Grenier <nathangrenier01@gmail.com>
2022-09-26 17:23:20 -0400

    Added test project.

---
+++ test/src/test/Test.java
@@ -0,0 +1,25 @@
+package test;
+import java.util.Scanner;
+
+public class Test {
+
+	public static void main(String[] args) {
+		/*
+		int mil50 = 50000000;
+		long value = 1000L * mil50;
+
+		double price = 4.35;
+		double quantity = 1000000;
+		double total = price / quantity;
+		*/
+		Scanner input = new Scanner(System.in);
+		System.out.print("Enter your age: ");
+		int num1 = input.nextInt();
+		int num2 = input.nextInt();
+
+		
+		System.out.println(num1 + " " + num2);
+		input.close();
+	}
+
+}
