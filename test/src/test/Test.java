77134c5a782c4ff7c30b13da00ed1da7a94a11bb
NathanGrenier <nathangrenier01@gmail.com>
2022-10-07 11:28:47 -0400

    switch case test

--- test/src/test/Test.java
+++ test/src/test/Test.java
@@ -4,13 +4,27 @@ import java.util.Scanner;
 public class Test {
 
 	public static void main(String[] args) {
-		
-		Scanner scanner = new Scanner(System.in);
-		
-		String value = scanner.next();
-		String value2 = scanner.next();
-		
-		System.out.printf(value + "||" + value2);
-		//System.out.println(str.substring(4));
+
+
+		for (int i = 1; i<=5; i++) {
+			System.out.println("This: " + i);
+			switch (i) {	
+			
+			case 1:
+				System.out.println(i);
+				break;
+			
+			case 2:
+				System.out.println(i);
+				break;
+			
+			case 3:
+				System.out.println(i);
+				break;
+			default:
+				System.out.println("default: " + i);
+			}
 		}
+ 
+	}
 }
